69fe04864db1d067e42686ab0903aa8604f4cbe3
Steffen Bartsch <steff.bartsch@gmx.de>
2017-11-22 17:06:33 +0100

    rabbitmq and mongo runs on new server

--- Chatssytem-Backend/app.js
+++ Chatssytem-Backend/app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongo = require('mongodb').MongoClient;
+var amqp = require('amqplib/callback_api');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -44,3 +46,76 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+var db;
+var collections = {
+    ch_1: {},
+    ch_2: {}
+};
+
+
+connectMongo();
+connectRabbitMQ();
+
+function connectMongo() {
+    mongo.connect('mongodb://localhost:27017/rabbitDB', function (err, _db) {
+        console.log("Weeeh, I connected mongo database successfully");
+        db = _db;
+
+        // create collection with same name like message queue
+        db.createCollection("ch_1", function(err, res) {
+            if (err) throw err;
+            console.log("Collection ch_1 created!");
+        });
+
+        collections.ch_1 = db.collection('ch_1');
+    });
+}
+
+function connectRabbitMQ() {
+    amqp.connect('amqp://localhost', function (err, conn) {
+
+        conn.createChannel(function (err, ch) {
+            if (err) throw err;
+            var q = 'ch_1';
+
+            ch.assertQueue(q, {durable: true});
+            // ch.prefetch(1);
+            console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+            ch.consume(q, function (msg) {
+                var message = msg.content.toString();
+                storeMessage(message, q);
+                console.log(" [x] Received %s", message);
+                // ch.ack(msg);
+            }, {noAck: true} );
+        });
+    });
+}
+
+
+function storeMessage(message, channel) {
+    collections[channel].insert({name: 'Thaer', lname: 'Aldefai', message: message})
+}
+
+// todo move to route: channel
+// app.get('/channel/:id', function (req, res) {
+//     var id = req.params.id;
+//     var col = collections['ch_' + id];
+//     console.log('Collection: ', col);
+//     col.find().toArray(function (err, channel) {
+//         res.send(channel);
+//     });
+// });
+
+// todo move to route: channel
+// app.get('/channel/:name/:message', function (req, res) {
+//     var msg = {
+//         name: req.params.name,
+//         message: req.params.message
+//     };
+//     // store name and message in MongoDB
+//     // ch_1_collection.insert(msg);
+//     res.send(msg);
+// });
+
