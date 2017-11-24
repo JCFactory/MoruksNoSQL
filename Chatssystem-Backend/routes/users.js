91f75767b841739eeb280100d5b8402cf5912dfe
Inan Bayram <inan.bayram@stud.hs-mannheim.de>
2017-11-24 18:53:49 +0100

    Structure added

--- Chatssystem-Backend/routes/users.js
+++ Chatssystem-Backend/routes/users.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+/**
+ * Get user information by id
+ */
+router.get('/:id', function (req, res, next) {
+
+});
+
+
+/**
+ * Login user
+ */
+router.post('/login', function (req, res, next) {
+
+});
+
 module.exports = router;
