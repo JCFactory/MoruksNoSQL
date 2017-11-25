af025745eb59d881b491a172151bf93fa78080ba
Steffen Bartsch <steff.bartsch@gmx.de>
2017-11-25 21:40:02 +0100

    split app in login and chat

--- morukChat/src/app/app.module.ts
+++ morukChat/src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
-import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
-
 import { AlertModule } from 'ngx-bootstrap';
 
+import { AppComponent } from './app.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { LoginComponent } from './components/login/login.component';
+import { RouterModule, Routes } from "@angular/router";
+
+const appRoutes: Routes
+  = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'chat',
+    component: ChatComponent
+  },
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    LoginComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     HttpClientModule,
     AlertModule.forRoot()
---
+++ morukChat/src/app/components/chat/chat.component.ts
@@ -0,0 +1,74 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+// import {AlertModule} from 'ngx-bootstrap';
+import {User} from "../../classes/user";
+
+const INITIAL_CHANNEL = 'ThaerTube';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.scss']
+})
+export class ChatComponent implements OnInit {
+
+  title = 'app';
+  results: Object;
+  currentChannel = INITIAL_CHANNEL;
+  user: User;
+  channels = [
+    {name: INITIAL_CHANNEL},
+    {name: 'General'},
+    {name: 'Default'}
+  ];
+
+
+  // TODO das ganze ist schlecht programmier sollte aber fürn Anfang reichen!
+  constructor(private http: HttpClient) {
+
+  }
+
+
+  ngOnInit(): void {
+    this.currentChannel = 'INITIAL_CHANNEL';
+    this.getChat('INITIAL_CHANNEL');
+  }
+
+
+  updateChat(event): void {
+    console.log(event);
+    this.currentChannel = event;
+    this.getChat(event);
+
+    console.log(this);
+  }
+
+
+  sendMessage(message): void {
+
+    const body = {
+      channel: this.currentChannel,
+      message: message
+    };
+
+
+    this.http.post('http://localhost:3000/channels/message', body).subscribe();
+
+    setTimeout( () => {
+      this.getChat(this.currentChannel);
+    }, 750);
+
+
+  }
+
+  getChat(channelName): void {
+
+    this.http.get('http://localhost:3000/channels/' + channelName).subscribe(data => {
+      // Read the result field from the JSON response.
+      this.results = data;
+      console.log(data);
+    });
+
+  }
+
+}
