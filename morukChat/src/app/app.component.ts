bc7bc7741fc9319d430db88436566a7c89045165
Steffen Bartsch <steff.bartsch@gmx.de>
2017-11-25 13:16:39 +0100

    input-field frontend

--- morukChat/src/app/app.component.ts
+++ morukChat/src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Moruk Chat';
+  // message: string = '';
+
+  output: any;
+
+  constructor() {
+
+  }
+
+  sendMessage(event: any): void {
+    this.output = event.target.value;
+  }
 }
